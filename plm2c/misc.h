8181c3a1329327669fa81074601e76890b30c0c1
David Bremner <dbremner@gmail.com>
2015-10-23 22:49:25 -0700

    raise warning level and fix all implicit function definition warnings

--- plm2c/misc.h
+++ plm2c/misc.h
@@ -41,13 +41,34 @@ typedef	unsigned char	BOOLEAN;
 
 #define NONE	0
 
-typedef struct TOKEN TOKEN;
-typedef struct DECL_MEMBER DECL_MEMBER;
-typedef struct DECL DECL;
+//FIXME unsnarl these
+#include "cvt.h" 
+#include "defs.h"
+#include "struct.h"
 //FIXME this saves including a protos.h file in *.c
 
 //context.c
+int find_member_symbol(TOKEN *symbol, DECL_MEMBER *decl_ptr, DECL_ID **decl_id);
+int find_list_symbol(TOKEN *symbol, DECL_MEMBER *decl_ptr, DECL_MEMBER **decl_found, DECL_ID **decl_id);
+int find_symbol(TOKEN *symbol, DECL_MEMBER **decl_found, DECL_ID **decl_id);
+void add_to_context(DECL_MEMBER *decl);
+void add_decl_to_context(DECL *decl);
+void new_context(int type, TOKEN *name);
+void pop_context(void);
 void init_context(void);
+
+//control.c
+void parse_control(void);
+
+//convert.c
+int parse_statement(TOKEN *first_token);
+int parse_new_statement(void);
+void parse_file(void);
+
+//declare.c
+void element_copy(DECL_MEMBER *src, DECL_MEMBER *dest);
+void parse_declare(TOKEN *first_token);
+int parse_type(DECL_MEMBER *el_ptr, TOKEN *token);
 
 //declout.c
 void out_pointer(TOKEN *name, BOOLEAN use_parens);
@@ -60,6 +81,7 @@ void out_decl(DECL *decl);
 //error.c
 void parse_error(char *error_string);
 void parse_warning(char *warning_string);
+void control_error(char *error_string);
 
 //io.c
 void out_data(char *string, int length);
@@ -79,9 +101,53 @@ void out_init(void);
 void out_str_const(char *str_ptr, int len);
 void cvt_octal(TOKEN *token, char octal_string[]);
 
+//main.c
+void cvt_file(char *file_name);
+
 //mem.c
-void *getmem(size_t);
+void *get_mem(size_t size);
+void get_context_ptr(CONTEXT **context);
 void get_token_ptr(TOKEN **token);
+void get_var_ptr(DECL_ID **var);
+void free_var_list(DECL_ID *list_ptr);
+void get_element_ptr(DECL_MEMBER **element);
+void free_decl_list(DECL_MEMBER *element);
+void get_param_ptr(PARAM_LIST **param);
+void free_param_list(PARAM_LIST *param_list);
+void get_decl_ptr(DECL **decl);
+void free_decl(DECL *decl_list);
+
+//parse.c
+void get_param_list(PARAM_LIST **param_head);
+void parse_param_list(PARAM_LIST *param_list, DECL **decl_list, DECL **extra_decl_list);
+void parse_till(int type, TOKEN *token);
+void parse_till_end(TOKEN *token);
+void parse_to_end(void);
+void check_eol(void);
+int parse_simple_variable(TOKEN *token, TOKEN *next_token);
+void out_ident(TOKEN *ident, DECL_MEMBER *decl, DECL_ID *decl_id);
+int parse_member(TOKEN *token, DECL_MEMBER *decl, DECL_ID *decl_id);
+int parse_variable(TOKEN *token, DECL_MEMBER **var_decl, DECL_ID **var_decl_id);
+int check_cvt_id(TOKEN *token, CVT_ID *cvt_id, char **cvt_string);
+int parse_function(TOKEN *token);
+int parse_expression(TOKEN *token);
+void parse_do(TOKEN *first_token);
+void parse_end(TOKEN *first_token);
+void parse_if(TOKEN *first_token);
+void parse_then(void);
+void parse_else(TOKEN *first_token);
+void parse_goto(TOKEN *first_token);
+void parse_go(TOKEN *first_token);
+void parse_call(TOKEN *first_token);
+void parse_return(TOKEN *first_token);
+void parse_identifier(TOKEN *first_token);
+void parse_label(void);
+void parse_eol(TOKEN *first_token);
+void parse_int_ctl(TOKEN *first_token);
+void parse_outport(void);
+void parse_output(TOKEN *first_token);
+void parse_outword(TOKEN *first_token);
+void parse_outhword(TOKEN *first_token);
 
 //token.c
 int get_token(TOKEN	*token);
